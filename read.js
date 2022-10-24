8deaf93343d283d1ef60b026000e784570505dc1
jennjunod <hello@teachjenntech.com>
2022-10-24 16:50:59 -0600

    read.js updates

--- read.js
+++ read.js
@@ -2,14 +2,16 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-
-  async function main() {
+async function main() {
     // ... you will write your Prisma Client queries here
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
+    const allUsers = await prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
+      },
+    })
+    console.dir(allUsers, { depth: null })
   }
-
-
 main()
   .then(async () => {
     await prisma.$disconnect()
